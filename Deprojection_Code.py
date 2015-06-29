fbce0e30e7d320cefd34fe549d0d8430cf1b3eee
cailmdaley <cailmdaley@gmail.com>
2015-06-29 16:28:15 -0400

    Added line at y=0, labeled real and imaginary plots seperately, allowed user to specify image filename, added in feature that deletes files of any extension w/ image filename before creating bm, mp, etc. files, added option for user to specify the channels they want to look at

--- Deprojection_Code.py
+++ Deprojection_Code.py
@@ -20,9 +20,10 @@ plt.close()
 #datatype = raw_input('Please enter \'line\' for line emission data or \'cont\' for continuum data: ')
 #outputtype = raw_input('Please enter \'rad\' for radial averaging, \'im\' for imaging, or \'both\' for both: ')
 #if outputtype in ('im', 'both'):
-#   image = raw_input('Please enter the filename you would like your image(s) to have, including the .uvf extension: ')
+#   image = raw_input('Please enter the filename you would like your image(s) to have, not including the filetype: ')
 #   if datatype == 'line':
-#      imchans = input('Please enter the channels you would like to image in standard line miriad format, usually of the form  \'number of channels, starting channel index, 1, step size\': ')    
+#      imchans = 'line=channel,' + raw_input('Please enter the channels you would like to image in standard line miriad format, of the form  \'number of channels,starting channel index,channel width,step size.\' Indexing starts at 1: ')    
+#       chandims = 'nxy=' + raw_input('Please enter the dimensions of the channel maps to be shown on each page, in the form \'columns,rows\': ')
 #if outputtype in ('rad', 'both'):
 #   if dataype == 'line':
 #       chansin =input('Please enter the indices of the channel(s) you would like to average, keeping in mind that indexing starts at 0 rather than one. For example, one might enter \'5,12\' if they wanted channels 6 through 12. If you would only like to average one channnel, please enter the index of the channel and then the index of the next channel (e.g. \'2,3\') ')
@@ -47,11 +48,13 @@ inc=math.radians(79.1)
 model='.uvf'
 modpa=math.radians(108.5)
 modinc=math.radians(79.1)
-image='49cetichannel.uvf'
+image='49cetichannelmap'
 deltara=0
 deltadec=0
 xlim = 300
 datatype= 'line'
+imchans = 'line=channel,'+'100,1,1,1'
+chandims = 'nxy=6,3'
     
 
 
@@ -67,50 +70,50 @@ visnum=len(u)
 
 
 #Reading in line emission visibilitites//weights and calculating phase shifts
-if datatype == 'line':
-    chans = range(2,6)
-    imchans = 5,10,1,1
-    rlxx = rlimwt[:,0,0,0,chans,0,0]
-    rlyy = rlimwt[:,0,0,0,chans,1,0]
-    imxx = rlimwt[:,0,0,0,chans,0,1]
-    imyy = rlimwt[:,0,0,0,chans,1,1]
-    wtxx = rlimwt[:,0,0,0,chans,0,2]
-    wtyy = rlimwt[:,0,0,0,chans,1,2]
-    rl = np.array((rlxx*wtxx+rlyy*wtyy)/(wtxx+wtyy))
-    im = np.array((imxx*wtxx+imyy*wtyy)/(wtxx+wtyy))
-    wt = 4.0/(1.0/wtxx + 1.0/wtyy)
-    amp=np.sqrt(rl**2+im**2)
+if outputtype in ('rad', 'both'):
+    if datatype == 'line':
+        chans = range(0,250)
+        rlxx = rlimwt[:,0,0,0,chans,0,0]
+        rlyy = rlimwt[:,0,0,0,chans,1,0]
+        imxx = rlimwt[:,0,0,0,chans,0,1]
+        imyy = rlimwt[:,0,0,0,chans,1,1]
+        wtxx = rlimwt[:,0,0,0,chans,0,2]
+        wtyy = rlimwt[:,0,0,0,chans,1,2]
+        rl = np.array((rlxx*wtxx+rlyy*wtyy)/(wtxx+wtyy))
+        im = np.array((imxx*wtxx+imyy*wtyy)/(wtxx+wtyy))
+        wt = 4.0/(1.0/wtxx + 1.0/wtyy)
+        amp=np.sqrt(rl**2+im**2)
 
-    deltaphase = 2*math.pi*(u*deltara + v*deltadec)
-    deltaphase = (np.tile(deltaphase, (len(chans), 1))).T
-    phase = np.arctan(im/rl)+deltaphase
-    rlcor = np.cos(phase)*amp
-    rlcor[np.where( rl<0 )] = -1.0*rlcor[np.where( rl<0 )]
-    imcor = np.sin(phase)*amp
-    imcor[np.where( rl<0)] = -1.0*imcor[np.where( rl<0 )]
+        deltaphase = 2*math.pi*(u*deltara + v*deltadec)
+        deltaphase = (np.tile(deltaphase, (len(chans), 1))).T
+        phase = np.arctan(im/rl)+deltaphase
+        rlcor = np.cos(phase)*amp
+        rlcor[np.where( rl<0 )] = -1.0*rlcor[np.where( rl<0 )]
+        imcor = np.sin(phase)*amp
+        imcor[np.where( rl<0)] = -1.0*imcor[np.where( rl<0 )]
 
 
-#Reading in continuum visibilitites//weights and calculating phase shifts
-if datatype == 'cont':
-    chans = header['NAXIS4']
-    rlxx = rlimwt[:,0,0,0,0,0]
-    rlyy = rlimwt[:,0,0,0,1,0]
-    imxx = rlimwt[:,0,0,0,0,1]
-    imyy = rlimwt[:,0,0,0,1,1]
-    wtxx = rlimwt[:,0,0,0,0,2]
-    wtyy = rlimwt[:,0,0,0,1,2]
-    rl = (rlxx*wtxx+rlyy*wtyy)/(wtxx+wtyy)
-    im = (imxx*wtxx+imyy*wtyy)/(wtxx+wtyy)
-    wt = 4.0/(1.0/wtxx + 1.0/wtyy)
-    amp=np.sqrt(rl**2+im**2)
+    #Reading in continuum visibilitites//weights and calculating phase shifts
+    if datatype == 'cont':
+        chans = header['NAXIS4']
+        rlxx = rlimwt[:,0,0,0,0,0]
+        rlyy = rlimwt[:,0,0,0,1,0]
+        imxx = rlimwt[:,0,0,0,0,1]
+        imyy = rlimwt[:,0,0,0,1,1]
+        wtxx = rlimwt[:,0,0,0,0,2]
+        wtyy = rlimwt[:,0,0,0,1,2]
+        rl = (rlxx*wtxx+rlyy*wtyy)/(wtxx+wtyy)
+        im = (imxx*wtxx+imyy*wtyy)/(wtxx+wtyy)
+        wt = 4.0/(1.0/wtxx + 1.0/wtyy)
+        amp=np.sqrt(rl**2+im**2)
 
-    deltaphase = 2*math.pi*(u*deltara + v*deltadec)
-    phase = np.arctan(im/rl)+deltaphase
-    rlcor = np.cos(phase)*amp
-    rlcor[np.where( rl<0)] = -1.0*rlcor
-    imcor = np.sin(phase)*amp
-    imcor[np.where( rl<0)] = -1.0*imcor
-ampcor = np.sqrt(rlcor**2+imcor**2)
+        deltaphase = 2*math.pi*(u*deltara + v*deltadec)
+        phase = np.arctan(im/rl)+deltaphase
+        rlcor = np.cos(phase)*amp
+        rlcor[np.where( rl<0)] = -1.0*rlcor
+        imcor = np.sin(phase)*amp
+        imcor[np.where( rl<0)] = -1.0*imcor
+    ampcor = np.sqrt(rlcor**2+imcor**2)
 
 
 #Rotation of (u,v) coordinates back to PA=0 and deprojection
@@ -205,7 +208,7 @@ if outputtype in ('rad', 'both'):
         modrlavg = np.zeros(bins)
         modimavg = np.zeros(bins)
         for i in range(bins-1):
-            if np.size(np.where( (moddepdist>=moddistrange[i]) & (moddepdist<=moddistrange[i+1]) ))==0:
+            if np.size(np.wherdfits.writetoe( (moddepdfits.writetodist>=moddistrange[i]) & (moddepdist<=moddistrange[i+1]) ))==0:
                 modrlavg[i]=0
                 modimavg[i]=0
             else:
@@ -217,16 +220,19 @@ if outputtype in ('rad', 'both'):
 #Plotting
     plt.subplot(211)
     plt.errorbar( distrange, rlavg*1e03, yerr=rlSEM*1e03, marker='.', fmt=' ' )
+    plt.axhline()
     if len(model)>4:
         plt.plot(moddistrange, modrlavg*1e03) 
-    plt.title('Flux as a Function of Deprojected Distance')
+    plt.title('Real Flux as a Function of Deprojected Distance')
     plt.xlabel('$k\lambda$')
     plt.ylabel('mJy')
 
     plt.subplot(212)
     plt.errorbar( distrange, imavg*1e03, yerr=imSEM*1e03, marker='.', fmt=' ' )
+    plt.axhline()
     if len(model)>4:
         plt.plot(moddistrange, modimavg*1e03) 
+    plt.title('Imaginary Flux as a Function of Deprojected Distance')
     plt.xlabel('$k\lambda$')
     plt.ylabel('mJy')
     #plt.savefig('49cetibinned.pdf')
@@ -236,13 +242,14 @@ if outputtype in ('rad', 'both'):
 if outputtype in ('im', 'both'):
     data['UU'] = u2
     data['VV'] = v2
-    dfits.writeto('deprojected_disk_image.uvf')
-    call("fits in=deprojected_disk_image.uvf op=uvin out=deprojected_disk_image.vis", shell=True)
+    call('rm -rf '+image+'.*', shell=True)
+    dfits.writeto(''+image+'.uvf')
+    call('fits in='+image+'.uvf op=uvin out='+image+'.vis', shell=True)
     if datatype == 'cont':
-        call("invert vis=deprojected_disk_image.vis map=deprojected_disk_image.mp beam=deprojected_disk_image.bm cell=0.3 imsize=256 options=systemp,mfs robust=2", shell=True)
+        call('invert vis='+image+'.vis map='+image+'.mp beam='+image+'.bm cell=0.3 imsize=256 options=systemp,mfs robust=2', shell=True)
     if datatype == 'line':
-        call("invert vis=deprojected_disk_image.vis map=deprojected_disk_image.mp beam=deprojected_disk_image.bm cell=0.3 imsize=256 line=channel,0,250,1,1 robust=2", shell=True)
-    call("clean map=deprojected_disk_image.mp beam=deprojected_disk_image.bm out=deprojected_disk_image.cl niters=300", shell=True)
-    call("restor map=deprojected_disk_image.mp beam=deprojected_disk_image.bm model=deprojected_disk_image.cl out=deprojected_disk_image.cm", shell=True)
-    call("cgdisp in=deprojected_disk_image.cm device=/xs labtyp=arcsec beamtyp=b,l,4", shell=True)
+        call('invert vis='+image+'.vis map='+image+'.mp beam='+image+'.bm cell=0.3 imsize=256 '+imchans+' robust=2', shell=True)
+    call('clean map='+image+'.mp beam='+image+'.bm out='+image+'.cl niters=300', shell=True)
+    call('restor map='+image+'.mp beam='+image+'.bm model='+image+'.cl out='+image+'.cm', shell=True)
+    call('cgdisp in='+image+'.cm '+chandims+' device=/xs labtyp=arcsec beamtyp=b,l,4', shell=True)
 
